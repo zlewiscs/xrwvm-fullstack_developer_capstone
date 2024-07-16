9d717b81f3c68065043f36385a72c3a29f9f18c4
Lewis <102457683+zlewiscs@users.noreply.github.com>
2024-07-16 10:11:39 +1000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -17,7 +17,7 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake,
-                                 on_delete\
+                                 on_delete
                                  =models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
